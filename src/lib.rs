760982f2806aa08044c30951fae7d2f8cb0fd169
Joel Palmer <joelpalmer@gmail.com>
2019-06-21 12:50:24 -0500

    remove Break value and Output type

--- src/lib.rs
+++ src/lib.rs
@@ -5,38 +5,46 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
 
-pub enum LoopResult<T, E> {
+pub enum LoopResult<E> {
     Continue,
-    Break(T),
+    Break,
     Error(E),
 }
 
 pub trait Cancellable {
-    type Output;
     type Error;
-    fn for_each(&mut self) -> LoopResult<Self::Output, ::Error>;
+    fn for_each(&mut self) -> LoopResult<Self::Error>;
 
     fn run(&mut self) -> Result<(), Self::Error> {
         loop {
             match self.for_each() {
                 LoopResult::Continue => {}
-                LoopResult::Break(v) => return Ok(v),
+                LoopResult::Break => break,
                 LoopResult::Error(e) => return Err(e),
             }
         }
+        Ok(())
     }
 
-    fn spawn(self) -> Handle<Self::Output, Self::Error>
+    fn spawn(self) -> Handle<Self::Error>
     where
         Self: Send,
         Self::Error: Send,
-        Self::Output: Send,
     {
         let keep_running = Arc::new(AtomicBool::new(true));
-        {
+        let jh = {
             let keep_running = keep_running.clone();
-            let jh = thread::spawn(move || {});
-        }
+            thread::spawn(move || {
+                while keep_running.load(Ordering::SeqCst) {
+                    match self.for_each() {
+                        LoopResult::Continue => {}
+                        LoopResult::Break => break,
+                        LoopResult::Error(e) => return Err(e),
+                    }
+                }
+                Ok(())
+            })
+        };
         let h = Handle::new();
         unimplemented!();
     }
